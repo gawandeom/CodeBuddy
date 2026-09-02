d18304a6f6d1638c18f3614ca100b7529c3474a3
gawandeom <gawandeom11@gmail.com>
2026-09-02 17:36:07 +0530

    feat: add file read/write operations and LangChain tools

---
+++ src/tools.ts
@@ -0,0 +1,30 @@
+import { tool } from "langchain"
+import { readFile, WriteFile } from "./fileops.js"
+import * as z from "zod"
+
+
+
+export const readFileTool =tool (({filePath})=>{
+    try {
+        return readFile(filePath)
+    } catch (error:any) {
+        console.log(`Error: ${error.message}`)
+    }
+},{
+    name:"read_file",
+    description:"Read the content of the file",
+    schema:z.object({
+        filePath:z.string()
+    })
+})
+
+export const writeFileTool =tool (({filePath,content})=>{
+  return WriteFile(filePath,content)
+},{
+    name:"write_file",
+    description:"writes the content in the file",
+    schema:z.object({
+        filePath:z.string(),
+        content:z.string()
+    })
+})
